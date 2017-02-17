e8709c73a7fa5694d5367b8979278efb1f680834
Santhosh Singh <santhosh.si@HCL.COM>
2017-02-17 14:26:26 +0000

    CRM-562  CRM Entities and Fileds Mapping done in CRM Service Class

--- Main/Source/Tc.Crm.WebJobs/Tc.Crm.WebJob.AllocateResortTeam/Services/CrmService.cs
+++ Main/Source/Tc.Crm.WebJobs/Tc.Crm.WebJob.AllocateResortTeam/Services/CrmService.cs
@@ -18,7 +18,7 @@ namespace Tc.Crm.WebJob.AllocateResortTeam
         IOrganizationService organizationService;
         IConfigurationService configurationService;
         ILogger logger;
-        public CrmService(IConfigurationService configurationService,ILogger logger)
+        public CrmService(IConfigurationService configurationService, ILogger logger)
         {
             this.configurationService = configurationService;
             this.organizationService = GetOrganizationService();
@@ -30,7 +30,7 @@ namespace Tc.Crm.WebJob.AllocateResortTeam
             throw new NotImplementedException();
         }
 
-        public  EntityCollection RetrieveMultipleRecords(string entityName, string[] columns, string[] filterKeys, string[] filterValues, IOrganizationService service)
+        public EntityCollection RetrieveMultipleRecords(string entityName, string[] columns, string[] filterKeys, string[] filterValues, IOrganizationService service)
         {
             var query = new QueryExpression(entityName);
             query.ColumnSet = new ColumnSet(columns);
@@ -54,9 +54,9 @@ namespace Tc.Crm.WebJob.AllocateResortTeam
 
         public EntityCollection RetrieveMultipleRecordsFetchXml(string Query, IOrganizationService service)
         {
-            //paging has to  impliment 
+            //paging has to be implimented 
             FetchExpression fetch = new FetchExpression(Query);
-            return service.RetrieveMultiple(fetch);          
+            return service.RetrieveMultiple(fetch);
 
         }
         public EntityCollection GetRecordsUsingQuery(QueryExpression queryExpr, IOrganizationService service)
@@ -93,7 +93,7 @@ namespace Tc.Crm.WebJob.AllocateResortTeam
         }
 
 
-        public  ExecuteMultipleResponse BulkUpdate(EntityCollection entityCollection ,IOrganizationService service)
+        public ExecuteMultipleResponse BulkUpdate(EntityCollection entityCollection, IOrganizationService service)
         {
             ExecuteMultipleRequest request = new ExecuteMultipleRequest()
             {
@@ -129,6 +129,8 @@ namespace Tc.Crm.WebJob.AllocateResortTeam
             throw new NotImplementedException();
         }
 
+
+
         public void Dispose()
         {
             Dispose(true);
@@ -157,7 +159,71 @@ namespace Tc.Crm.WebJob.AllocateResortTeam
                 else
                     obj = null;
             }
-                
+
         }
     }
+
+    public static class EntityName
+    {
+
+        public const string Booking = "tc_booking";
+        public const string BookingAccommodation = "tc_bookingaccommodation";
+        public const string CustomerBookingRole = "tc_customerbookingrole";
+        public const string Hotel = "tc_hotel";
+        public const string Team = "team";
+        public const string User = "systemuser";
+
+    }
+
+    public static class Attributes
+    {
+        public class Booking
+        {
+            public const string BookingId = "tc_bookingid";
+            public const string Owner = "ownerid";
+            public const string Name = "tc_name";
+
+        }
+
+        public class BookingAccommodation
+        {
+            public const string BookingAccommodationid = "tc_bookingaccommodationid";
+            public const string BookingId = "tc_bookingid";
+            public const string EndDateandTime = "tc_enddateandtime";
+            public const string HotelId = "tc_hotelid";
+            public const string NumberofParticipants = "tc_numberofparticipants";
+            public const string NumberofRooms = "tc_numberofrooms";
+            public const string SourceMarketHotelCode = "tc_sourcemarkethotelcode";
+            public const string SourceMarketHotelName = "tc_sourcemarkethotelname";
+            public const string StartDateandTime = "tc_startdateandtime";
+            
+        }
+
+        public class CustomerBookingRole
+        {
+            public const string BookingId = "tc_bookingid";
+            public const string Customer = "tc_customer";
+            public const string Role = "tc_customerbookingrole";
+            public const string CustomerBookingRoleId = "tc_customerbookingroleid";
+            public const string Name = "tc_name";
+        }
+
+        public class Hotel
+        {
+           
+            public const string HotelId = "tc_hotelid";
+            public const string LocationId = "tc_locationid";
+            public const string MasterHotelID = "tc_masterhotelid";
+            public const string Name = "tc_name";
+            public const string SourceMarketHotelID = "tc_sourcemarkethotelid";
+            public const string ResortTeam = "tc_teamid";
+            
+        }
+        public class Team
+            {
+            public const string TeamId = "teamid";
+            public const string Name = "name";
+            }
+    }
 }
+
