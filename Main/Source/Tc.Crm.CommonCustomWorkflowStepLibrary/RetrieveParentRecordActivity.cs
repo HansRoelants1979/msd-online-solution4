856d2a7f4335062de9e0c57c69644d605db8126d
Debabrata Majumder <debabrata.m@HCL.COM>
2017-03-23 15:51:59 +0000

    CRM 1061, finalized input and out argumentsNot required

--- Main/Source/Tc.Crm.CommonCustomWorkflowStepLibrary/RetrieveParentRecordActivity.cs
+++ Main/Source/Tc.Crm.CommonCustomWorkflowStepLibrary/RetrieveParentRecordActivity.cs
@@ -24,11 +24,11 @@ namespace Tc.Crm.CommonCustomWorkflowStepLibrary
             IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
             try
             {
-                var sourceEntity = SourceEntity.Get<EntityReference>(executionContext);
-                var targetEntity = TargetEntity.Get<string>(executionContext);
-                var relationshipName = RelationshipName.Get<string>(executionContext);
-                ParentRecord.Set(executionContext, RetrieveParentRecord(sourceEntity, targetEntity, relationshipName, service));
-
+                //var sourceEntity = SourceEntity.Get<EntityReference>(executionContext);
+                //var targetEntity = TargetEntity.Get<string>(executionContext);
+                //var relationshipName = RelationshipName.Get<string>(executionContext);
+                //ParentRecord.Set(executionContext, RetrieveParentRecord(sourceEntity, targetEntity, relationshipName, service));
+                
             }
             catch (FaultException<OrganizationServiceFault> ex)
             {
@@ -45,49 +45,18 @@ namespace Tc.Crm.CommonCustomWorkflowStepLibrary
 
         }
 
-        static EntityReference RetrieveParentRecord(EntityReference SourceEntity, string TargetEntity, string RelationshipName, IOrganizationService service)
-        {
-            EntityReference parentEntity = null;
-            //EntityCollection ParentEntityRecords = null;
-            List<Entity> ParentEntityRecords = new List<Entity>();
+        
+        //starting entity should be the entity on which the workflow runs
+        //case||customerid;contact||parentaccountid;account||companyname
+        [Input("Expression")]
+        public string Expression { get; set; }
 
-            //the related entity we are going to retrieve
-            QueryExpression query = new QueryExpression();
-            query.EntityName = TargetEntity;
-            query.ColumnSet = new ColumnSet();
+        //entity reference - string
+        //optionset - string
+        [Output("ReturnValue")]
+        public string ReturnValue { get; set; }
 
-            //the relationship that links the primary to the target
-            Relationship relationship = new Relationship(RelationshipName);
-            relationship.PrimaryEntityRole = EntityRole.Referenced; //important if the relationship is self-referencing
 
-            //the query collection which forms the request
-            RelationshipQueryCollection relatedEntity = new RelationshipQueryCollection();
-            relatedEntity.Add(relationship, query);
-
-            //the request to get the primary entity with the related records
-            RetrieveRequest request = new RetrieveRequest();
-            request.RelatedEntitiesQuery = relatedEntity;
-            request.ColumnSet = new ColumnSet();
-            request.Target = new EntityReference(SourceEntity.LogicalName, SourceEntity.Id);
-
-            RetrieveResponse r = (RetrieveResponse)service.Execute(request);
-
-            //query the returned collection for the target entity 
-            ParentEntityRecords = r.Entity.RelatedEntities[relationship].Entities.Select(e => e).ToList();
-            
-            return parentEntity = new EntityReference(TargetEntity,ParentEntityRecords[0].Id );
-        }
-        [Input("EntityReference SourceEntity")]
-        public InArgument<EntityReference> SourceEntity { get; set; }
-
-        [Input("String TargetEntity")]
-        public InArgument<string> TargetEntity { get; set; }
-
-        [Input("String RelationshipName")]
-        public InArgument<string> RelationshipName { get; set; }
-
-        [Output("EntityReference ParentRecord")]
-        public OutArgument<EntityReference> ParentRecord { get; set; }
 
 
 
