b137198500f019ab73150059cfee37ee5138a996
Srinivasphani Yerubandi <srinivasphani.y@HCL.COM>
2017-03-24 20:01:45 +0000

    <CRM-850, updated survey response custom workflow to add additionalparameters as a valid JSON>as not bale to build solution due to issue in unit testing

--- Main/Source/Tc.Crm.CustomWorkflowSteps/ProcessSurvey/Services/SurveyResponseHelper.cs
+++ Main/Source/Tc.Crm.CustomWorkflowSteps/ProcessSurvey/Services/SurveyResponseHelper.cs
@@ -48,54 +48,72 @@ namespace Tc.Crm.CustomWorkflowSteps.ProcessSurvey.Services
         {
             trace.Trace("Processing PrepareAdditionalParameters - start");
             var additionalParameters = new StringBuilder();
-            additionalParameters.AppendLine("Id: " + response.Id);
+            additionalParameters.AppendLine("{");
+            additionalParameters.AppendLine(FormatJson("Id", response.Id, true));
             if (response.SurveyGatewayId != null)
-                additionalParameters.AppendLine("SurveyGatewayId: " + response.SurveyGatewayId);
+                additionalParameters.AppendLine(FormatJson("SurveyGatewayId", response.SurveyGatewayId, true));
             if (!string.IsNullOrWhiteSpace(response.SurveyGatewayName))
-                additionalParameters.AppendLine("SurveyGatewayName: " + response.SurveyGatewayName);
+                additionalParameters.AppendLine(FormatJson("SurveyGatewayName",response.SurveyGatewayName));
             if (!string.IsNullOrWhiteSpace(response.SurveyGatewayAlias))
-                additionalParameters.AppendLine("SurveyGatewayAlias: " + response.SurveyGatewayAlias);
+                additionalParameters.AppendLine(FormatJson("SurveyGatewayAlias", response.SurveyGatewayAlias));
             if (response.SurveyId != null)
-                additionalParameters.AppendLine("SurveyId: " + response.SurveyId);
+                additionalParameters.AppendLine(FormatJson("SurveyId", response.SurveyId, true));
             if (!string.IsNullOrWhiteSpace(response.SurveyName))
-                additionalParameters.AppendLine("SurveyName: " + response.SurveyName);
+                additionalParameters.AppendLine(FormatJson("SurveyName", response.SurveyName));
             if (!string.IsNullOrWhiteSpace(response.BeginTime))
-                additionalParameters.AppendLine("BeginTime: " + response.BeginTime);
+                additionalParameters.AppendLine(FormatJson("BeginTime", response.BeginTime));
             if (!string.IsNullOrWhiteSpace(response.DateOfService))
-                additionalParameters.AppendLine("DateOfService: " + response.DateOfService);
+                additionalParameters.AppendLine(FormatJson("DateOfService", response.DateOfService));
             if (!string.IsNullOrWhiteSpace(response.RedemptionCode))
-                additionalParameters.AppendLine("RedemptionCode: " + response.RedemptionCode);
-            additionalParameters.AppendLine("Minutes: " + response.Minutes);
-            additionalParameters.AppendLine("Complete: " + response.Complete);
-            additionalParameters.AppendLine("Read: " + response.Read);
+                additionalParameters.AppendLine(FormatJson("RedemptionCode", response.RedemptionCode));
+            additionalParameters.AppendLine(FormatJson("Minutes", response.Minutes, true));
+            additionalParameters.AppendLine(FormatJson("Complete", response.Complete.ToString().ToLower(), true));
+            additionalParameters.AppendLine(FormatJson("Read", response.Read.ToString().ToLower(), true));
             if (!string.IsNullOrWhiteSpace(response.IpAddress))
-                additionalParameters.AppendLine("IpAddress: " + response.IpAddress);
+                additionalParameters.AppendLine(FormatJson("IpAddress", response.IpAddress));
             if (!string.IsNullOrWhiteSpace(response.CookieUID))
-                additionalParameters.AppendLine("CookieUID: " + response.CookieUID);
+                additionalParameters.AppendLine(FormatJson("CookieUID", response.CookieUID));
             if (response.OfferId != null)
-                additionalParameters.AppendLine("OfferId: " + response.OfferId);
+                additionalParameters.AppendLine(FormatJson("OfferId", response.OfferId, true));
             if (!string.IsNullOrWhiteSpace(response.OfferName))
-                additionalParameters.AppendLine("OfferName: " + response.OfferName);
+                additionalParameters.AppendLine(FormatJson("OfferName", response.OfferName));
             if (!string.IsNullOrWhiteSpace(response.OfferCode))
-                additionalParameters.AppendLine("OfferCode: " + response.OfferCode);
+                additionalParameters.AppendLine(FormatJson("OfferCode", response.OfferCode));
             if (response.UnitId != null)
-                additionalParameters.AppendLine("UnitId: " + response.UnitId);
+                additionalParameters.AppendLine(FormatJson("UnitId", response.UnitId, true));
             if (!string.IsNullOrWhiteSpace(response.UnitExternalId))
-                additionalParameters.AppendLine("UnitExternalId: " + response.UnitExternalId);
+                additionalParameters.AppendLine(FormatJson("UnitExternalId", response.UnitExternalId));
             if (!string.IsNullOrWhiteSpace(response.UnitName))
-                additionalParameters.AppendLine("UnitName: " + response.UnitName);
+                additionalParameters.AppendLine(FormatJson("UnitName", response.UnitName));
             if (response.OrganizationId != null)
-                additionalParameters.AppendLine("OrganizationId: " + response.OrganizationId);
+                additionalParameters.AppendLine(FormatJson("OrganizationId", response.OrganizationId, true));
             if (!string.IsNullOrWhiteSpace(response.OrganizationName))
-                additionalParameters.AppendLine("OrganizationName: " + response.OrganizationName);
+                additionalParameters.AppendLine(FormatJson("OrganizationName", response.OrganizationName));
             if (!string.IsNullOrWhiteSpace(response.ExclusionReason))
-                additionalParameters.AppendLine("ExclusionReason: " + response.ExclusionReason);
+                additionalParameters.AppendLine(FormatJson("ExclusionReason", response.ExclusionReason));
             if (!string.IsNullOrWhiteSpace(response.Mode))
-                additionalParameters.AppendLine("Mode: " + response.Mode);
+                additionalParameters.AppendLine(FormatJson("Mode", response.Mode));
             if (!string.IsNullOrWhiteSpace(response.SurveyDescription))
-                additionalParameters.AppendLine("SurveyDescription: " + response.SurveyDescription);
+                additionalParameters.AppendLine(FormatJson("SurveyDescription", response.SurveyDescription));            
+            additionalParameters.AppendLine("}");
             trace.Trace("Processing PrepareAdditionalParameters - end");
-            return additionalParameters.ToString();
+            return additionalParameters.ToString().Remove(additionalParameters.ToString().Length - 6, 1);
+        }
+
+        private static string FormatJson(string fieldName, object fieldValue, bool isNotStringType)
+        {
+            var format = string.Empty;
+            if (!isNotStringType)
+                format = "\"" + fieldName + "\": \"" + fieldValue + "\",";
+            else
+                format = "\"" + fieldName + "\": " + fieldValue + ",";
+
+            return format;
+        }
+
+        private static string FormatJson(string fieldName, object fieldValue)
+        {
+            return FormatJson(fieldName, fieldValue, false);
         }
 
 
