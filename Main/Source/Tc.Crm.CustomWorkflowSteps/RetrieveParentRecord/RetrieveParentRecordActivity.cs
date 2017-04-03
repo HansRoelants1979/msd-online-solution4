90d8a6cd3ba2591320fd6cc0d51b2611077e752f
Debabrata Majumder <debabrata.m@HCL.COM>
2017-04-03 08:14:46 +0000

    Used the existing TC Key, merged the common utility workflow activity with the main

---
+++ Main/Source/Tc.Crm.CustomWorkflowSteps/RetrieveParentRecord/RetrieveParentRecordActivity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Activities;
+using System.Linq;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Messages;
+using System.Collections.Generic;
+using Tc.Crm.CustomWorkflowSteps.RetrieveParentRecord.Services;
+
+namespace Tc.Crm.CustomWorkflowSteps
+{
+    public class RetrieveParentRecordActivity : CodeActivity
+    {
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            //Create the tracing service
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+
+            //Create the context
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            try
+            {
+
+                
+                var expression = Expression.Get<string>(executionContext);
+                ReturnValue.Set(executionContext, RetrieveRecordProcessHelper.RetrieveParentRecord(expression, service,context));
+
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                throw new InvalidPluginExecutionException(ex.ToString());
+            }
+            catch (TimeoutException ex)
+            {
+                throw new InvalidPluginExecutionException(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.ToString());
+            }
+
+        }
+
+        
+
+        //starting entity should be the entity on which the workflow runs
+        //case||customerid;contact||parentaccountid;account||companyname
+
+        [Input("String Expression")]
+        public InArgument<string> Expression { get; set; }
+
+        //entity reference - string
+        //optionset - string
+
+        [Output("String ReturnValue")]
+        public OutArgument<string> ReturnValue { get; set; }
+
+
+
+
+    }
+}
