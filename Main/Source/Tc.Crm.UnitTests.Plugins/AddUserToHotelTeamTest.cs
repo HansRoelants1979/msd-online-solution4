c5c7021522bd83a5f6ff1c12ddde40311456d507
Srinivasphani Yerubandi <srinivasphani.y@HCL.COM>
2017-04-04 11:27:22 +0000

    <CRM-1002, Added unit test cases for CreateHotelTeam>Not able to build whole solution

---
+++ Main/Source/Tc.Crm.UnitTests.Plugins/AddUserToHotelTeamTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xrm.Sdk;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeXrmEasy;
+
+namespace Tc.Crm.UnitTests.Plugins
+{
+    [TestClass]
+    public class AddUserToHotelTeamTest
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPluginExecutionException))]
+        public void AddUserToHotelTeam()
+        { 
+            var context = new XrmFakedContext();
+            var cntxt = new XrmFakedPluginExecutionContext();           
+            var entRefcol = new EntityReferenceCollection();           
+            entRefcol.Add(new EntityReference("systemuser",Guid.NewGuid()));
+            cntxt.InputParameters = new ParameterCollection();
+            cntxt.InputParameters.Add("Relationship", "teammembership_association.");
+            cntxt.InputParameters.Add("Target", new EntityReference("team", Guid.NewGuid()));
+            cntxt.InputParameters.Add("RelatedEntities", entRefcol);
+            cntxt.MessageName = "Associate";
+            context.ExecutePluginWithConfigurations<Crm.Plugins.AddUserToHotelTeam>(cntxt, "IL,UK", "");          
+        }
+    }
+}
--- Main/Source/Tc.Crm.UnitTests.Plugins/CreateHotelOwnerTests.cs
+++ Main/Source/Tc.Crm.UnitTests.Plugins/CreateHotelOwnerTests.cs
@@ -32,6 +32,25 @@ namespace Tc.Crm.UnitTests.Plugins
             var target = new Microsoft.Xrm.Sdk.Entity("tc_hotel") { Id = Guid.NewGuid() };
             target["tc_name"] = "Hotel XXX";
             Assert.ThrowsException<InvalidPluginExecutionException>(() => context.ExecutePluginWithTarget<Crm.Plugins.CreateHotelOwner>(target));
+        }
+
+        [TestMethod]
+        public void CreateTeam()
+        {
+            var role = new Entity("role", Guid.NewGuid());
+            role["name"] = "Tc.Ids.Base";
+            role["businessunitid"] = Guid.Empty;           
+            var context = new XrmFakedContext();
+            context.Initialize(new List<Entity>() {
+            role
+            });
+            context.AddRelationship("teamroles_association", new XrmFakedRelationship("teamid", "roleid", "team", "role"));
+            var target = new Microsoft.Xrm.Sdk.Entity("tc_hotel") { Id = Guid.NewGuid() };
+            target["tc_name"] = "Hotel XXX";
+            context.ExecutePluginWithTarget<Crm.Plugins.CreateHotelOwner>(target);
+            var team = (from t in context.CreateQuery("team")
+                          select t).ToList();
+            Assert.IsTrue(team.Count == 1);
         }       
     }
 }
