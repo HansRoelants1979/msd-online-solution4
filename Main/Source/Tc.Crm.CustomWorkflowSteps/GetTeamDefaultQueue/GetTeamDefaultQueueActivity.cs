c1fc0f0a75bc285558ca14529cbbd85513601865
Santhosh Singh <santhosh.si@HCL.COM>
2017-05-16 18:40:14 +0000

    CRM-205...GetTeamDefaultQueue custom workflow code...

---
+++ Main/Source/Tc.Crm.CustomWorkflowSteps/GetTeamDefaultQueue/GetTeamDefaultQueueActivity.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tc.Crm.CustomWorkflowSteps.GetTeamDefaultQueue.Service;
+
+namespace Tc.Crm.CustomWorkflowSteps.GetTeamDefaultQueue
+{
+   public class GetTeamDefaultQueueActivity : CodeActivity
+    {
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService trace = executionContext.GetExtension<ITracingService>();
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            var sourceMarketId = SourceMarket.Get<EntityReference>(executionContext);
+
+            var teamDefaultQueueService = new GetTeamDefaultQueueService();
+
+            trace.Trace("getting team default queue by Source Market.");
+            if (sourceMarketId != null)
+            {
+                var response = teamDefaultQueueService.GetTeamDefaultQueue(sourceMarketId, service, trace);
+                if (response != null)
+                {
+                    executionContext.SetValue<EntityReference>(Queue, response);
+                }
+                else
+                    trace.Trace("response is null");
+
+                return;
+            }
+        }
+
+        [Input("SourceMarket")]
+        [ReferenceTarget("tc_country")]
+        public InArgument<EntityReference> SourceMarket { get; set; }
+
+        [Output("Queue")]
+        [ReferenceTarget("queue")]
+        public OutArgument<EntityReference> Queue { get; set; }
+
+    }
+}
---
+++ Main/Source/Tc.Crm.CustomWorkflowSteps/GetTeamDefaultQueue/Service/GetTeamDefaultQueueService.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tc.Crm.CustomWorkflowSteps.GetTeamDefaultQueue.Service
+{
+    public class GetTeamDefaultQueueService
+    {
+        public EntityReference GetTeamDefaultQueue(EntityReference sourceMarket, IOrganizationService service, ITracingService trace)
+        {
+            EntityReference teamDefaultQueueDetails = null;
+
+            if (sourceMarket == null) return null;
+            if (service == null) return null;
+            if (trace == null) return null;
+
+            trace.Trace("GetTeamDefaultQueue - start");
+            teamDefaultQueueDetails = GetteamDefaultQueueDetails(sourceMarket, service, trace);
+            if (teamDefaultQueueDetails == null)
+            {
+                trace.Trace("teamDefaultQueueDetails are null.");
+                return null;
+            }
+
+            trace.Trace("GetTeamDefaultQueue - end");
+            return teamDefaultQueueDetails;
+        }
+
+        private EntityReference GetteamDefaultQueueDetails(EntityReference sourceMarket, IOrganizationService service, ITracingService trace)
+        {
+            EntityReference teamDefaultQueue = null;
+            if (sourceMarket == null) return null;
+            if (service == null) return null;
+            if (trace == null) return null;
+
+            trace.Trace("GetteamDefaultQueueDetails - start");
+            var fetchXml = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='true'>
+                           <entity name='team'>
+                            <attribute name='name' />
+                            <attribute name='businessunitid' />
+                            <attribute name='teamid' />
+                            <attribute name='teamtype' />
+                            <attribute name='isdefault' />
+                            <attribute name='queueid' />
+                              <order attribute='name' descending='false' />
+                            <filter type='and'>
+                              <condition attribute='isdefault' operator='eq' value='1' />
+                            </filter>
+                            <link-entity name='businessunit' from='businessunitid' to='businessunitid' alias='ac'>
+                            <link-entity name='tc_country' from='tc_sourcemarketbusinessunitid' to='businessunitid' alias='ad'>
+                            <filter type='and'>
+                             <condition attribute='tc_countryid' operator='eq' uiname='{0}' uitype='tc_country' value='{1}' />
+                            </filter>
+                            </link-entity>
+                            </link-entity>
+                           </entity>
+                          </fetch>";
+            fetchXml = string.Format(fetchXml, sourceMarket.Name, sourceMarket.Id);
+            trace.Trace(fetchXml);
+            var query = new FetchExpression(fetchXml);
+            trace.Trace("Calling retrieve multiple of service.");
+            var response = service.RetrieveMultiple(query);
+
+            if (response == null || response.Entities == null || response.Entities.Count == 0)
+            {
+                trace.Trace("response is null or response.Entities is null or count is null");
+                return null;
+            }
+            
+            trace.Trace("Team record retrieved.");
+
+            if (response.Entities[0].Contains("queueid") && response.Entities[0]["queueid"] != null)
+            {
+                teamDefaultQueue = ((EntityReference)(response.Entities[0]["queueid"]));
+            }
+
+            trace.Trace("GetteamDefaultQueueDetails - end");
+            return teamDefaultQueue;
+        }
+    }
+}
