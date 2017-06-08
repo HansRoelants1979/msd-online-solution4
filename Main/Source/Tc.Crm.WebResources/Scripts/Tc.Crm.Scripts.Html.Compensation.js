a727f572ac59983f2e48a30aaec7488d312d93f8
Santhosh Singh <santhosh.si@HCL.COM>
2017-06-08 17:16:16 +0000

    CRM-146--changes suggested by Srini as review comments

---
+++ Main/Source/Tc.Crm.WebResources/Scripts/Tc.Crm.Scripts.Html.Compensation.js
@@ -0,0 +1,41 @@
+﻿if (typeof (Tc) === "undefined") {
+    Tc = {
+        __namespace: true
+    };
+}
+if (typeof (Tc.Crm) === "undefined") {
+    Tc.Crm = {
+        __namespace: true
+    };
+}
+if (typeof (Tc.Crm.Scripts) === "undefined") {
+    Tc.Crm.Scripts = {
+        __namespace: true
+    };
+}
+if (typeof (Tc.Crm.Scripts.Html) === "undefined") {
+    Tc.Crm.Scripts.Html = {
+        __namespace: true
+    };
+    Tc.Crm.Scripts.Html.Compensation = (function () {
+        "use strict";
+        function onLoadGetCompensationSignature() {
+
+            var sigDataAttr = parent.Xrm.Page.getAttribute("tc_compensationsignature");
+            if (sigDataAttr != null && sigDataAttr != "undefined") {
+                var sigData = sigDataAttr.getValue();
+                if (sigData != null) {
+                    $("#SignatureImg").attr("src", sigData);
+                }
+                else {
+                    $("body").html("No signature captured.");
+                }
+            }
+
+        }
+
+        return {
+            OnLoad: onLoadGetCompensationSignature
+        };
+    })();
+}
