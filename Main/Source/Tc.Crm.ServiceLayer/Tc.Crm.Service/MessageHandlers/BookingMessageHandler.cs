1cb06209ff352c44f1b1201c398a008c0b377f75
Debabrata Majumder <debabrata.m@HCL.COM>
2017-06-15 11:39:18 +0000

    CRM-2222, removed payload logging, just logging the booking number

--- Main/Source/Tc.Crm.ServiceLayer/Tc.Crm.Service/MessageHandlers/BookingMessageHandler.cs
+++ Main/Source/Tc.Crm.ServiceLayer/Tc.Crm.Service/MessageHandlers/BookingMessageHandler.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using Tc.Crm.Service.Models;
 
 namespace Tc.Crm.Service.MessageHandlers
 {
@@ -16,9 +20,48 @@ namespace Tc.Crm.Service.MessageHandlers
         {
             var header = request.Headers.ToString();
             var t = request.Content.ReadAsStringAsync();
+            var content = t.Result;
+
             Trace.TraceInformation(header);
-            Trace.TraceInformation(t.Result);
+
+            LogBookingNumberFromBody(content);
             return base.SendAsync(request, cancellationToken);
         }
+
+
+        private void LogBookingNumberFromBody(string content)
+        {
+            try
+            {
+                if (!content.Contains("\"booking\"")) return;
+                var bookingInfo = DeserializeContent(content);
+                if (bookingInfo == null
+                    || bookingInfo.BookingForLog == null
+                    || bookingInfo.BookingForLog.BookingIdentifierForLog == null
+                    || string.IsNullOrWhiteSpace(bookingInfo.BookingForLog.BookingIdentifierForLog.BookingNumberForLog)) return;
+                Trace.TraceInformation(bookingInfo.BookingForLog.BookingIdentifierForLog.BookingNumberForLog);
+            }
+            catch (Exception)
+            {
+                Trace.TraceError("Error in Booking Message Handler.");
+            }
+           
+        }
+
+        public static BookingInformationForLog DeserializeContent(string json)
+        {
+            if (string.IsNullOrWhiteSpace(json)) return null;
+
+            BookingInformationForLog bookingInfo = new BookingInformationForLog();
+            using (MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                DataContractJsonSerializer deSerializer = new DataContractJsonSerializer(bookingInfo.GetType());
+                bookingInfo = deSerializer.ReadObject(memoryStream) as BookingInformationForLog;
+            }
+
+            return bookingInfo;
+
+        }
     }
+
 }
