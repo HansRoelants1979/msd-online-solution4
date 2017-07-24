37c33b10d44dd320f5c8b4c013ab41daecc49502
viktor.kanaylo <viktor.kanaylo@HCLO365.onmicrosoft.com>
2017-07-24 15:35:29 +0000

    CRM-2343: added scriptLoader to scripts

--- Main/Source/Tc.Crm.WebResources/Scripts/Tc.Crm.Scripts.Events.Case.js
+++ Main/Source/Tc.Crm.WebResources/Scripts/Tc.Crm.Scripts.Events.Case.js
@@ -1,4 +1,44 @@
-﻿if (typeof (Tc) === "undefined") {
+﻿var scriptLoader = scriptLoader || {
+    delayedLoads : [],
+    load: function (name, requires, script) {
+        window._loadedScripts = window._loadedScripts || { };
+        // Check for loaded scripts, if not all loaded then register delayed Load
+        if (requires == null || requires.length == 0 || scriptLoader.areLoaded(requires)) {
+            scriptLoader.runScript(name, script);
+        }
+        else {
+            // Register an onload check
+            scriptLoader.delayedLoads.push({ name : name, requires: requires, script : script });
+        }
+    },
+    runScript: function (name, script) {
+        script.call(window);
+        window._loadedScripts[name]= true;
+        scriptLoader.onScriptLoaded(name);
+    },
+    onScriptLoaded: function (name) {
+        // Check for any registered delayed Loads
+        scriptLoader.delayedLoads.forEach(function (script) {
+            if (script.loaded == null && scriptLoader.areLoaded(script.requires)) {
+                script.loaded = true;
+                scriptLoader.runScript(script.name, script.script);
+            }
+        });
+    },
+    areLoaded: function (requires) {
+        var allLoaded = true;
+        for (var i = 0; i < requires.length; i++) {
+            allLoaded = allLoaded && (window._loadedScripts[requires[i]]!= null);
+            if (!allLoaded)
+                break;
+        }
+        return allLoaded;
+    }
+};
+scriptLoader.load("Tc.Crm.Scripts.Events.Case", ["Tc.Crm.Scripts.Library.Contact"], function () {
+// start script
+
+if (typeof (Tc) === "undefined") {
     Tc = {
         __namespace: true
     };
@@ -419,3 +459,7 @@ Tc.Crm.Scripts.Events.Case = (function () {
         }
     };
 })();
+
+// end script
+console.log('loaded events.case');
+});
