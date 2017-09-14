c7031667a69bbc1dfb95041b7eb1ca03454e2512
Pavlo Naida <pavlo.naida@HCLO365.onmicrosoft.com>
2017-09-14 12:56:27 +0000

    Changes to customer form scripts after playback session

--- Main/Source/Tc.Crm.WebResources/Scripts/Tc.Crm.Scripts.Events.Contact.Retail.js
+++ Main/Source/Tc.Crm.WebResources/Scripts/Tc.Crm.Scripts.Events.Contact.Retail.js
@@ -37,216 +37,248 @@
 };
 scriptLoader.load("Tc.Crm.Scripts.Events.Contact.Retail", ["Tc.Crm.Scripts.Utils.Validation"], function () {
 
-// start script
-if (typeof (Tc) === "undefined") {
-    Tc = {
-        __namespace: true
-    };
-}
-if (typeof (Tc.Crm) === "undefined") {
-    Tc.Crm = {
-        __namespace: true
-    };
-}
-if (typeof (Tc.Crm.Scripts) === "undefined") {
-    Tc.Crm.Scripts = {
-        __namespace: true
-    };
-}
-if (typeof (Tc.Crm.Scripts.Events) === "undefined") {
-    Tc.Crm.Scripts.Events = {
-        __namespace: true
-    };
-}
-
-if (typeof (Tc.Crm.Scripts.Events.Contact) === "undefined") {
-    Tc.Crm.Scripts.Events.Contact = {
-        __namespace: true
-    };
-}
-
-Tc.Crm.Scripts.Events.Contact.Retail = (function () {
-    "use strict";
-    var Attributes = {
-        Salutation: "tc_salutation",
-        FirstName: "firstname",
-        LastName: "lastname",
-        BirthDate: "birthdate",      
-        ContactChangeReason: "tc_keycontactchangereason",
-        Telephone1: "telephone1",
-        Telephone2: "telephone2",
-        Telephone3: "telephone3"
+    // start script
+    if (typeof (Tc) === "undefined") {
+        Tc = {
+            __namespace: true
+        };
     }
-    var FormMode = {
-      Create: 1,
-      Update: 2
+    if (typeof (Tc.Crm) === "undefined") {
+        Tc.Crm = {
+            __namespace: true
+        };
     }
-    var Tabs = {
-        CustomerHolidays: "tab_customersholidays",
-        Indicators: "tab_indicators",
-        CustomerPreferences: "tab_customerpreferences",
-        AdditionalCustomerDetails: "tab_additionalcustomerdetails",
-        MarketingConsent: "tab_marketingconsent",
-        PastHolidays: "tab_pastholidays",
-        Cases: "tab_cases",
-        ContactPreference: "tab_contactpreference",
-        ExternalLogin: "tab_externallogin",
-        PastBookings: "tab_bookings"
+    if (typeof (Tc.Crm.Scripts) === "undefined") {
+        Tc.Crm.Scripts = {
+            __namespace: true
+        };
     }
-    var Sections = {
-    }
-    
-    function onPhoneChanged(context) {
-        var attribute = context.getEventSource();
-        Tc.Crm.Scripts.Utils.Validation.ValidatePhoneNumber(attribute.getName());
+    if (typeof (Tc.Crm.Scripts.Events) === "undefined") {
+        Tc.Crm.Scripts.Events = {
+            __namespace: true
+        };
     }
 
-    function hideTabsSections() {
-      hideTab(Tabs.CustomerHolidays);
-      hideTab(Tabs.Indicators);
-      hideTab(Tabs.CustomerPreferences);
-      hideTab(Tabs.AdditionalCustomerDetails);
-      hideTab(Tabs.MarketingConsent);
-      hideTab(Tabs.PastHolidays);
-      hideTab(Tabs.Cases);
-      hideTab(Tabs.ExternalLogin);
-      hideTab(Tabs.PastBookings);
+    if (typeof (Tc.Crm.Scripts.Events.Contact) === "undefined") {
+        Tc.Crm.Scripts.Events.Contact = {
+            __namespace: true
+        };
     }
 
-    function hideTab(tabName) {
-      var tab = Xrm.Page.ui.tabs.get(tabName);
-      if (tab) {
-          tab.setVisible(false);
-      }
-    }
-
-    var onCustomerKeyInformationUpdate = function () {
-        if (isCustomerKeyInformationUpdated())
-            showContactChangeReason();
-        else
-            hideContactChangeReason();
-      };
-
-    var hideContactChangeReason = function () {
-        if (Xrm.Page.getAttribute(Attributes.ContactChangeReason)) {
-            Xrm.Page.getAttribute(Attributes.ContactChangeReason).setRequiredLevel("none");
-            Xrm.Page.getControl(Attributes.ContactChangeReason).setVisible(false);
+    Tc.Crm.Scripts.Events.Contact.Retail = (function () {
+        "use strict";
+        var Attributes = {
+            Salutation: "tc_salutation",
+            FirstName: "firstname",
+            LastName: "lastname",
+            BirthDate: "birthdate",
+            ContactChangeReason: "tc_keycontactchangereason",
+            Telephone1: "telephone1",
+            Telephone2: "telephone2",
+            Telephone3: "telephone3"
         }
-    };
-
-    var showContactChangeReason = function () {
-        if (isControlVisible(Attributes.ContactChangeReason))
-            return;
-        if (Xrm.Page.getAttribute(Attributes.ContactChangeReason)) {
-            Xrm.Page.getAttribute(Attributes.ContactChangeReason).setValue(null);
-            Xrm.Page.getControl(Attributes.ContactChangeReason).setVisible(true);
-            Xrm.Page.getAttribute(Attributes.ContactChangeReason).setRequiredLevel("required");
+        var FormMode = {
+            Create: 1,
+            Update: 2
         }
-    };
-
-    var isCustomerKeyInformationUpdated = function () {
-        if (isFormTypeUpdate())
-        {
-            var titleUpdated = getIsDirty(Attributes.Salutation);
-            var firstNameUpdated = getIsDirty(Attributes.FirstName);
-            var lastNameUpdated = getIsDirty(Attributes.LastName);
-            var dateOfBirthUpdated = getIsDirty(Attributes.BirthDate);
-            if (titleUpdated || firstNameUpdated || lastNameUpdated || dateOfBirthUpdated)
-                return true;
+        var Tabs = {
+            CustomerHolidays: "tab_customersholidays",
+            Indicators: "tab_indicators",
+            CustomerPreferences: "tab_customerpreferences",
+            AdditionalCustomerDetails: "tab_additionalcustomerdetails",
+            MarketingConsent: "tab_marketingconsent",
+            PastHolidays: "tab_pastholidays",
+            Cases: "tab_cases",
+            ContactPreference: "tab_contactpreference",
+            ExternalLogin: "tab_externallogin",
+            PastBookings: "tab_bookings",
+            Activities: "tab_activitiesnotes",
+            CustomerDetails: "SUMMARY_TAB"
         }
-        return false;
-    };
-
-    var onLoad = function () {
-        Tc.Crm.Scripts.Utils.Validation.ValidatePhoneNumber(Attributes.Telephone1);
-        Tc.Crm.Scripts.Utils.Validation.ValidatePhoneNumber(Attributes.Telephone2);
-        Tc.Crm.Scripts.Utils.Validation.ValidatePhoneNumber(Attributes.Telephone3);
-        if (isFormTypeCreate()) {
-            hideTabsSections();
+        var Sections = {
         }
-    }
-  
-    var onSave = function (econtext) {
-        if (isCustomerKeyInformationUpdated()) {
-            var eventArgs = econtext.getEventArgs();            
-            if (eventArgs.getSaveMode() != 70)//AutoSave
-            {   
-                if (!confirm("The changes you have requested will update this customer record only"))
-                    noCloseCallback(econtext);
-            }
-            else {
-                eventArgs.preventDefault();                
+
+        function onPhoneChanged(context) {
+            var attribute = context.getEventSource();
+            Tc.Crm.Scripts.Utils.Validation.ValidatePhoneNumber(attribute.getName());
+        }
+
+        function hideTabsSections() {
+            setTabVisibility(Tabs.CustomerHolidays, false);
+            setTabVisibility(Tabs.Indicators, false);
+            setTabVisibility(Tabs.CustomerPreferences, false);
+            setTabVisibility(Tabs.AdditionalCustomerDetails, false);
+            setTabVisibility(Tabs.MarketingConsent, false);
+            setTabVisibility(Tabs.PastHolidays, false);
+            setTabVisibility(Tabs.Cases, false);
+            setTabVisibility(Tabs.ExternalLogin, false);
+            setTabVisibility(Tabs.PastBookings, false);
+            setTabVisibility(Tabs.Activities, false);
+        }
+
+        function showTabsSections() {
+            setTabVisibility(Tabs.CustomerHolidays, true);
+            setTabVisibility(Tabs.Indicators, true);
+            setTabVisibility(Tabs.CustomerPreferences, true);
+            setTabVisibility(Tabs.AdditionalCustomerDetails, true);
+            setTabVisibility(Tabs.MarketingConsent, true);
+            setTabVisibility(Tabs.PastHolidays, true);
+            setTabVisibility(Tabs.Cases, true);
+            setTabVisibility(Tabs.ExternalLogin, true);
+            setTabVisibility(Tabs.PastBookings, true);
+            setTabVisibility(Tabs.Activities, true);
+        }
+
+        function setTabVisibility(tabName, isVisible) {
+            var tab = Xrm.Page.ui.tabs.get(tabName);
+            if (tab) {
+                tab.setVisible(isVisible);
             }
         }
-    };
-   
-    var noCloseCallback = function (econtext)
-    {
-        Xrm.Utility.alertDialog("You need to create a new customer record, do not save the changes you have made");
-        var eventArgs = econtext.getEventArgs();
-        eventArgs.preventDefault();
-        Xrm.Page.data.refresh(false);
-        //Xrm.Page.getAttribute(Attributes.BirthDate).setSubmitMode('never');
-    };
-    
 
-    var isFormTypeUpdate = function () {
-        var formType = Xrm.Page.ui.getFormType();
-        return (formType === FormMode.Update)
-    };
-
-    var isFormTypeCreate = function () {
-        var formType = Xrm.Page.ui.getFormType();
-        return (formType === FormMode.Create)
-    };
-    
-    var getIsDirty = function (controlName) {
-        if (Xrm.Page.getAttribute(controlName))
-            return Xrm.Page.getAttribute(controlName).getIsDirty()
-        else
-            return false;
-    };
-
-    var isControlVisible = function (controlName) {
-        if (Xrm.Page.getControl(controlName))
-          return Xrm.Page.getControl(controlName).getVisible();
-        else
-            return false;
-    };
-    var onModifiedOnChanged = function () {
-        if (window.IsUSD == true) {
-
-            window.open("http://event/?eventname=RefreshFollowUp");
-
-        }
-    };
-
-
-    // public methods
-    return {
-        OnLoad: onLoad,
-        OnSave: function (econtext) {
-            var isValid = Tc.Crm.Scripts.Utils.Validation.ValidateGdprCompliance(context);
-            if (isValid) {
-                onSave(econtext);
+        function toggleTab(tabName, isExpanded) {
+            var tab = Xrm.Page.ui.tabs.get(tabName);
+            if (tab) {
+                tab.setVisible(true);
+                if (isExpanded) {
+                    tab.setDisplayState("expanded");
+                }
+                else {
+                    tab.setDisplayState("collapsed");
+                }
             }
-        },
-        OnPhoneChanged: function (context) {
-            if (context === null) {
-                console.log("Tc.Crm.Uk.Scripts.Events.Contact.OnPhoneChanged should be configured to pass execution context");
+        }
+
+        var onCustomerKeyInformationUpdate = function () {
+            if (isCustomerKeyInformationUpdated())
+                showContactChangeReason();
+            else
+                hideContactChangeReason();
+        };
+
+        var hideContactChangeReason = function () {
+            if (Xrm.Page.getAttribute(Attributes.ContactChangeReason)) {
+                Xrm.Page.getAttribute(Attributes.ContactChangeReason).setRequiredLevel("none");
+                Xrm.Page.getControl(Attributes.ContactChangeReason).setVisible(false);
+            }
+        };
+
+        var showContactChangeReason = function () {
+            if (isControlVisible(Attributes.ContactChangeReason))
                 return;
+            if (Xrm.Page.getAttribute(Attributes.ContactChangeReason)) {
+                Xrm.Page.getAttribute(Attributes.ContactChangeReason).setValue(null);
+                Xrm.Page.getControl(Attributes.ContactChangeReason).setVisible(true);
+                Xrm.Page.getAttribute(Attributes.ContactChangeReason).setRequiredLevel("required");
             }
-            onPhoneChanged(context);
-        },
-        OnCustomerKeyInformationUpdate: function () {
-            onCustomerKeyInformationUpdate();
-        },
-        OnModifiedOnChanged: function () {
-            onModifiedOnChanged();
-        }
-    };
-})();
+        };
 
-// end script
-console.log('loaded events.contact.retail');
+        var isCustomerKeyInformationUpdated = function () {
+            if (isFormTypeUpdate()) {
+                var titleUpdated = getIsDirty(Attributes.Salutation);
+                var firstNameUpdated = getIsDirty(Attributes.FirstName);
+                var lastNameUpdated = getIsDirty(Attributes.LastName);
+                var dateOfBirthUpdated = getIsDirty(Attributes.BirthDate);
+                if (titleUpdated || firstNameUpdated || lastNameUpdated || dateOfBirthUpdated)
+                    return true;
+            }
+            return false;
+        };
+
+        var onLoad = function () {
+            Tc.Crm.Scripts.Utils.Validation.ValidatePhoneNumber(Attributes.Telephone1);
+            Tc.Crm.Scripts.Utils.Validation.ValidatePhoneNumber(Attributes.Telephone2);
+            Tc.Crm.Scripts.Utils.Validation.ValidatePhoneNumber(Attributes.Telephone3);
+            if (isFormTypeCreate()) {
+                hideTabsSections();
+                toggleTab(Tabs.CustomerDetails, true);
+            }
+            if (isFormTypeUpdate()) {
+                showTabsSections();
+                toggleTab(Tabs.CustomerDetails, false);
+            }
+        }
+
+        var onSave = function (econtext) {
+            if (isCustomerKeyInformationUpdated()) {
+                var eventArgs = econtext.getEventArgs();
+                if (eventArgs.getSaveMode() != 70)//AutoSave
+                {
+                    if (!confirm("The changes you have requested will update this customer record only"))
+                        noCloseCallback(econtext);
+                }
+                else {
+                    eventArgs.preventDefault();
+                }
+            }
+        };
+
+        var noCloseCallback = function (econtext) {
+            Xrm.Utility.alertDialog("You need to create a new customer record, do not save the changes you have made");
+            var eventArgs = econtext.getEventArgs();
+            eventArgs.preventDefault();
+            Xrm.Page.data.refresh(false);
+            //Xrm.Page.getAttribute(Attributes.BirthDate).setSubmitMode('never');
+        };
+
+
+        var isFormTypeUpdate = function () {
+            var formType = Xrm.Page.ui.getFormType();
+            return (formType === FormMode.Update)
+        };
+
+        var isFormTypeCreate = function () {
+            var formType = Xrm.Page.ui.getFormType();
+            return (formType === FormMode.Create)
+        };
+
+        var getIsDirty = function (controlName) {
+            if (Xrm.Page.getAttribute(controlName))
+                return Xrm.Page.getAttribute(controlName).getIsDirty()
+            else
+                return false;
+        };
+
+        var isControlVisible = function (controlName) {
+            if (Xrm.Page.getControl(controlName))
+                return Xrm.Page.getControl(controlName).getVisible();
+            else
+                return false;
+        };
+        var onModifiedOnChanged = function () {
+            if (window.IsUSD == true) {
+
+                window.open("http://event/?eventname=RefreshFollowUp");
+
+            }
+        };
+
+
+        // public methods
+        return {
+            OnLoad: onLoad,
+            OnSave: function (context) {
+                var isValid = Tc.Crm.Scripts.Utils.Validation.ValidateGdprCompliance(context);
+                if (isValid) {
+                    onSave(context);
+                }
+            },
+            OnPhoneChanged: function (context) {
+                if (context === null) {
+                    console.log("Tc.Crm.Uk.Scripts.Events.Contact.OnPhoneChanged should be configured to pass execution context");
+                    return;
+                }
+                onPhoneChanged(context);
+            },
+            OnCustomerKeyInformationUpdate: function () {
+                onCustomerKeyInformationUpdate();
+            },
+            OnModifiedOnChanged: function () {
+                onModifiedOnChanged();
+            }
+        };
+    })();
+
+    // end script
+    console.log('loaded events.contact.retail');
 });
