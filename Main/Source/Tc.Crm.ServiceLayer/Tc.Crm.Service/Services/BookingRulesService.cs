f27c961b0a51cd22d0b7ec16c0db4ef3b0c66c60
Bharathkumar Devulapalli <bharathkumar-d@HCL.COM>
2017-10-05 11:01:02 +0000

    Checking in BookingRulesService Stub methods

---
+++ Main/Source/Tc.Crm.ServiceLayer/Tc.Crm.Service/Services/BookingRulesService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tc.Crm.Common.Models;
+
+namespace Tc.Crm.Service.Services
+{
+    public class BookingRulesService
+    {
+        public bool IsOnTourorTCVAtCoreBooking(Booking booking)
+        {
+            return false;
+        }
+
+        public bool IsOnTourBooking(Booking booking)
+        {
+            return false;
+        }
+
+        public bool IsBookingConsultationEmpty(Booking booking)
+        {
+            return false;
+        }
+
+        public Entity MatchingBooking(Booking booking)
+        {
+            return new Entity();
+        }
+
+        public bool IsSameCustomer(Booking booking,Entity msdBooking)
+        {
+            return false;
+        }
+
+        public Entity MatchingCustomer(Booking booking)
+        {
+            return new Entity();
+        }
+
+
+    }
+}
