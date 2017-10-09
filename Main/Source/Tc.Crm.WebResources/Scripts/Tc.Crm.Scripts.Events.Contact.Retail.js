0bbb29ba11bd9fe2e69e65c537f2c5762aa16600
viktor.kanaylo <viktor.kanaylo@HCLO365.onmicrosoft.com>
2017-10-09 10:33:21 +0000

    Removed dependancy to Library.Contact from Events.Contact.Retail js

--- Main/Source/Tc.Crm.WebResources/Scripts/Tc.Crm.Scripts.Events.Contact.Retail.js
+++ Main/Source/Tc.Crm.WebResources/Scripts/Tc.Crm.Scripts.Events.Contact.Retail.js
@@ -35,7 +35,7 @@
         return allLoaded;
     }
 };
-scriptLoader.load("Tc.Crm.Scripts.Events.Contact.Retail", ["Tc.Crm.Scripts.Utils.Validation", "Tc.Crm.Scripts.Library.Contact"], function () {
+scriptLoader.load("Tc.Crm.Scripts.Events.Contact.Retail", ["Tc.Crm.Scripts.Utils.Validation"], function () {
 
     // start script
     if (typeof (Tc) === "undefined") {
