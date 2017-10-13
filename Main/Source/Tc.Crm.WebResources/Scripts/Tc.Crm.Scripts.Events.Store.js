b308bf1214813bc2ce6bcf68b8c081b9a66b8b2c
Srinivasphani Yerubandi <srinivasphani.y@HCL.COM>
2017-10-13 13:20:20 +0000

    [CRM-3624, Technical - Don't allow creation of travel planners and quotes for closed store]

---
+++ Main/Source/Tc.Crm.WebResources/Scripts/Tc.Crm.Scripts.Events.Store.js
@@ -0,0 +1,46 @@
+﻿if (typeof (Tc) === "undefined") {
+    Tc = {
+        __namespace: true
+    };
+}
+if (typeof (Tc.Crm) === "undefined") {
+    Tc.Crm = {
+        __namespace: true
+    };
+}
+if (typeof (Tc.Crm.Scripts) === "undefined") {
+    Tc.Crm.Scripts = {
+        __namespace: true
+    };
+}
+if (typeof (Tc.Crm.Scripts.Events) === "undefined") {
+    Tc.Crm.Scripts.Events = {
+        __namespace: true
+    };
+}
+Tc.Crm.Scripts.Events.Store = (function () {
+    "use strict";
+
+    var Attributes = {
+        StoreClosed: "tc_storeclosed"
+    }
+
+    var getControlValue = function (controlName) {
+        if (Xrm.Page.getAttribute(controlName))
+            return Xrm.Page.getAttribute(controlName).getValue();
+        else
+            return null;
+    }
+
+    var showNewTravelPlanButton = function () {
+        var storeClosed = getControlValue(Attributes.StoreClosed);
+        return ((storeClosed != null && storeClosed == true) ? false : true);
+    };
+
+    return {       
+        ShowNewTravelPlanButton: function () {
+            return showNewTravelPlanButton();
+        }
+
+    };
+})();
